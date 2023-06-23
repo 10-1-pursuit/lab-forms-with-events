bf7f788c136d06dbc725d153ccfc3550f817e400
Jeremy Montes <jeremymontes@pursuit.org>
2023-06-23 14:00:20 -0400

    created li and all forms

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Code your solution!")
+const lists = document.querySelector("ul")
+const paragraph = document.querySelector("p")
+
+const forms = document.querySelector("form")
+
+
+
+forms.addEventListener("submit", (event)=>{
+
+event.preventDefault()
+
+const item = document.createElement("li")
+
+item.innerText = event.target["list-item"].value
+
+if(event.target["list-item"].value === ""){
+    paragraph.innerText = 'Error. Todo cannot be empty';
+    
+}else{
+    lists.append(item)
+}
+
+event.target.reset()
+});
+
+
+
+
+let items = document.querySelectorAll("li")
+
+for(let singleItem of items){
+
+singleItem.addEventListener("click",(event)=>{
+event.target.style["text-decoration"] = "line-through;"
+})
+
+}
