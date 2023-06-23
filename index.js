1fb2fc3640c006d0ef306db4273dc7dc46fdcd87
christophersanchez <christophersanchez@pursuit.org>
2023-06-23 12:48:09 -0400

    Finally able to make my app do what it's supposed to do. I had my script outside the <body> 🙈. Will try for bonus 🫡

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Code your solution!")
+//console.log("Code your solution!")
+
+// Get the form, input, error message, and todo list elements
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const errorMessage = document.getElementById('error-message');
+const todoList = document.getElementById('todo-list');
+
+// Function to create a new todo item and add it to the list
+function addTodoItem(text) {
+    // Create a new li element
+    const todoItem = document.createElement('li');
+
+    // Set the text of the li element
+    todoItem.textContent = text;
+
+    // Add event listener to mark the item as completed when clicked
+    todoItem.addEventListener('click', function () {
+        todoItem.classList.toggle('completed');
+    });
+
+    // Append the li element to the todo list
+    todoList.appendChild(todoItem);
+}
+
+// Event listener for the form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission from refreshing the page
+
+    const todoText = input.value.trim(); // Get the value of the input and remove leading/trailing whitespace
+
+    if (todoText !== '') {
+        addTodoItem(todoText); // Add the todo item
+        input.value = ''; // Clear the input field
+        errorMessage.textContent = ''; // Clear the error message
+    } else {
+        errorMessage.textContent = 'Error. Todo cannot be empty'; // Display error message
+    }
+});
