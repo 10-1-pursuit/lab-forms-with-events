5bc2fa6d0737e470db877d68c83162940bdf9874
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-06-25 19:20:13 -0400

    My submit button does not add item to list

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log("Code your solution!")
+const dontBodyShameMe = document.querySelector("body")
+const headingOne = document.querySelector("h1")
+headingOne.textContent = "My To-Dos"
+const newForm = document.querySelector("form")
+const submit = document.getElementById("submitButton")
+const unorderedList = document.querySelector("ul")
+
+const toDoListDiv = document.querySelector("div")
+
+const newItemForList = document.querySelector("p")
+
+
+
+
+
+
+// toDoListDiv.append(newForm)
+
+newForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const liEvent = document.createElement("li")
+    liEvent.classList.add("newList")
+    liEvent.innerText = event.target["toDoList"].value
+
+    liEvent.addEventListener("click", (event) => {
+        liEvent.style.texDecorationLine = "line-through";
+
+    })
+
+    liEvent.addEventListener("mouseover", (event) => {
+        liEvent.style.color = "black";
+
+    })
+
+    unorderedList.append(liEvent)
+    setTimeout(newForm.reset(), 3000)
+
+})
+
+
+const listItems = document.getElementsByClassName("newList")
+
+console.log(listItems)
+
+
+
+
