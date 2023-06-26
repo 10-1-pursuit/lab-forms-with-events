35aa08727393b07301cbd5e1ddfe38fc00b7f7ed
anickacodes <anickalewis@pursuit.org>
2023-06-26 00:55:50 -0400

    created and appended submit buttons using js

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 document.querySelector("title").innerText = "Just Another Day";
 
-const header = document.createElement('header')
-header.innerText = 'Have a great day & spread Grace';
-header.classList.add('header')
-document.querySelector('body').appendChild(header)
+const br = document.createElement("br");
+const ul = document.createElement("ul");
 
+const header = document.createElement("header");
+header.innerText = "Have a great day & spread Grace";
+header.classList.add("header");
+document.querySelector("body").appendChild(header);
 
 const h1 = document.querySelector("h1");
 h1.innerText = "06/24/23 Task List";
 
-
-document.createElement('label')
-
 const form = document.createElement("form");
-form.setAttribute("method", "post")
-form.setAttribute("action", "submit.php")
+form.classList.add("list");
+form.setAttribute("method", "post");
+form.setAttribute("action", "submit.php");
 document.querySelector("body").append(form);
+form.style.boxShadow = "80px 44.44px 410px 444px teal";
 
-const list = document.createElement("p");
-document.querySelector("form").append(list);
+const fieldset = document.createElement("fieldset")
+fieldset.innerText = 'TotD: Go easy on me =]'
+form.appendChild(fieldset);
 
-list.classList.add('list');
-list.innerText = 'TotD: Go easy on me =]'
-list.style.boxShadow = '10px 4.44px 10px 3px teal';
+let input = document.createElement("input");
+input.setAttribute("type", "text");
+//input.setAttribute("name", "text");
+input.setAttribute("placeholder", "Task");
 
+form.appendChild(input);
+form.appendChild(br.cloneNode());
 
+let s = document.createElement("input");
+s.setAttribute("type", "submit");
+s.setAttribute("value", "Submit");
+
+form.append(s);
