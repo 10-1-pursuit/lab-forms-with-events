7d53af1884ddb3082ee2bea374013ffba524894b
anickacodes <anickalewis@pursuit.org>
2023-06-26 04:58:42 -0400

    all events working 😭

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 document.title = "Just Another Day";
 
 const br = document.createElement("br");
-
+const body = document.querySelector("body");
 const header = document.createElement("header");
 header.textContent = "Have a Blessed day & Give Grace";
 header.classList.add("header");
-document.querySelector("body").appendChild(header);
+body.appendChild(header);
 
 const h1 = document.querySelector("h1");
 h1.textContent = "06/24/23 Task List";
@@ -14,7 +14,7 @@ const form = document.createElement("form");
 form.classList.add("list");
 form.method = "post";
 form.action = "submit.php";
-document.querySelector("body").appendChild(form);
+body.appendChild(form);
 form.style.boxShadow = "90px 84.44px 900px 400px teal";
 
 const fieldset = document.createElement("fieldset");
@@ -42,8 +42,12 @@ form.append(label2);
 
 const ul = document.createElement("ul");
 ul.style.display = "none";
-//do something w this so its invisible upon page load
-//addeventlistener
+ul.addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("completed");
+    updateCompletionStatus();
+  }
+});
 
 const div = document.createElement("div");
 div.classList.add("Tasks-List");
@@ -55,15 +59,49 @@ form.addEventListener("submit", (event) => {
 
   const inputTask = document.getElementById("task");
   const task = inputTask.value.trim();
-  
-  if (task !== '') {
- const li = document.createElement('li')
- li.textContent = task 
- ul.appendChild(li)
- ul.style.display = 'block'
 
- inputTask.value = '';
-  
+  if (task !== "") {
+    const li = document.createElement("li");
+    li.textContent = task;
+    ul.appendChild(li);
+    ul.style.display = "block";
+
+    inputTask.value = "";
+    errorMessage.textContent = "";
+  } else {
+    errorMessage.textContent =
+      "OOPS! Did you forget something? No task specified🥹 ";
   }
+
   event.target.reset();
 });
+
+const p = document.createElement("p");
+p.id = "error-message";
+body.appendChild(p);
+
+const errorMessage = document.getElementById("error-message");
+
+const button = document.createElement("button");
+button.id = "completion-btn";
+button.textContent = "All tasks completed";
+button.style.display = "none";
+
+body.appendChild(button);
+
+const completedBtn = document.getElementById("completion-btn");
+
+// completedBtn.addEventListener("click", (event) => {
+function updateCompletionStatus() {
+  const lis = ul.querySelectorAll("li");
+  const tasksDone = [...lis].every((li) => li.classList.contains("completed"));
+
+  if (tasksDone) {
+    button.style.display = "block";
+    //alert("Tasks Successfully Completed");
+  } else {
+    button.style.display = "none";
+    //alert("Tasks Still Awaiting Completion");
+  }
+}
+//updateCompletionStatus() 
