cd0ef977b937f7e459c8fabdeabe01a154f64ec0
Fig <bethanytrr@gmail.com>
2023-06-26 23:46:21 -0400

    Main section of lab completed, no bonus tasks yet. Adjusted template for html page with default boilerplate.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Code your solution!")
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const list = document.querySelector('ul');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const text = input.value;
+    const submitted = document.createElement('li');
+    submitted.textContent = text;
+
+    if (text === '') {
+        const error = document.createElement('p');
+        error.textContent = `Error! Cannot be empty.`;
+        list.append(error);
+    } else {
+        list.append(submitted);
+    }
+
+    list.addEventListener('click', (event) => {
+        submitted.style.textDecoration = 'line-through solid';
+    });
+});
+
